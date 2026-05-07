94e5fb33ab7b25d493177c0e195d121d0ff3318b
DVMendoza1988 <danielvincentmendoza@pm.me>
2026-05-07 12:26:20 -0500

    Secure: Added input validation to main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("System Online: Hello DevSecOps!")
+	// len() checks how many items are in os.Args
+	if len(os.Args) < 2 {
+		fmt.Println("Error: Please provide a username.")
+		fmt.Println("Usage: go run main.go [name]")
+		os.Exit(1) // Exit with 1 to signal a failure to the OS
+	}
+
+	fmt.Println("System Online: Hello", os.Args[1])
 }
