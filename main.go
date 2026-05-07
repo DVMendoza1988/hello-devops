fd280ba191ac586b3a62c37b330a0b74f4a3c8a4
DVMendoza1988 <danielvincentmendoza@pm.me>
2026-05-07 15:57:53 -0500

    Milestone 2: Introduction of strconv and error handling

--- main.go
+++ main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv" // New Package: String Conversion
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Error: Missing argument.")
+		fmt.Println("Usage: go run main.go [number_of_servers]")
 		os.Exit(1)
 	}
 
-	// Declare a variable named 'user' and assign it the value from Args
-	var user string = os.Args[1]
+	// 1. Arguments always come in as Strings.
+	//2. We must convert the string to an Integer (int) to do math.
+	input := os.Args[1]
+	serverCount, err := strconv.Atoi(input)
 
-	fmt.Printf("Access Granted: Welcome, %s\n", user)
+	// In go, we handle errors IMMEDIATELY.
+
+	if err != nil {
+		fmt.Println("Error: Please provide a whole number.")
+		os.Exit(1)
+	}
+
+	const coresPerServer = 4 // Constants are declared with 'const' and cannot be changed.
+	totalCores := serverCount * coresPerServer
+	fmt.Printf("Infratructure report:\n")
+	fmt.Printf("- Servers %d\n", serverCount)
+	fmt.Printf("- Total CPU Cores Needed: %d\n", totalCores)
 }
