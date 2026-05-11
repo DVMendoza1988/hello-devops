cbb6a7f0157c15b50d624155ca2a9496512b2575
DVMendoza1988 <danielvincentmendoza@pm.me>
2026-05-11 13:53:44 -0500

    Milestone 5: Pointers and Methods

--- main.go
+++ main.go
@@ -2,60 +2,44 @@ package main
 
 import "fmt"
 
-// 1. THE BLUEPRINT
-// 'type' tells Go we are inventing a new shape.
-// 'struct' means it will be a collection of other variables.
 type Server struct {
 	Name      string
 	IPAddress string
-	Crashes   int
 	IsOnline  bool
+	Crashes   int
+}
+
+// 1. THE METHOD
+// Notice the special receiver block: (s *Server) BEFORE the function name.
+// This physicall attaches the 'Reboot' function to the Server blueprint.
+// * tells Go: "Do not make a copy. Use the Pointer to the actual memory."
+
+func (s *Server) Reboot() {
+	fmt.Printf("\n--- INITIATING REBOOT SEQUENCE FOR %s ---\n", s.Name)
+
+	// Because we used a pointer (*), this changes the REAL server.
+	s.IsOnline = true
+	s.Crashes = 0
+
+	fmt.Printf("SYSTEM MESSAGE: Boot successful.")
 }
 
 func main() {
-	fmt.Println("--- PROVISIONING NEW INFRASTRUCTURE ---")
-
-	// 2. BUILDING AN INSTANCE
-	// We use our 'Server' blueprint to create a specific machine.
-	// We use := to create and assign it to the variable 'frontendServer'.
-	frontendServer := Server{
-		Name:      "Web-Front-01",
-		IPAddress: "192.168.1.50",
-		Crashes:   0,
-		IsOnline:  true, // true means it is running!
+	// 2. PROVISION A DEAD SERVER
+	targetServer := Server{
+		Name:      "Payment-Gateway-01",
+		IPAddress: "172.16.254.1",
+		IsOnline:  false,
+		Crashes:   14,
 	}
-	dataBaseServer := Server{
-		Name:      "Customer-DB-01",
-		IPAddress: "10.0.0.5",
-		Crashes:   5,
-		IsOnline:  false, // false means it is down!
-	}
+	// 3. CHECK INITIAL STATE
+	fmt.Println("Initial State ->", targetServer.Name, "Online:", targetServer.IsOnline)
 
-	// 3. ACCESSING DATA (Dot Notation)
-	// We use a period '.' to look inside the struct and grab a specific field.
-	fmt.Println("Booting:", frontendServer.Name)
-	fmt.Println("IP:", frontendServer.IPAddress)
+	// 4. PRESS THE REBOOT BUTTON
+	// We use dot notation to trigger the method attached to this specific struct.
+	targetServer.Reboot()
 
-	fmt.Println("Booting:", dataBaseServer.Name)
-	fmt.Println("IP:", dataBaseServer.IPAddress)
-
-	// 4. THE SRE SCENARIO
-	// A power blip hits the data center. The server goes down.
-	fmt.Println("... POWER ANOMALY DETECTED ...")
-
-	// We use dot notation to modify the internal state of our struct.
-	frontendServer.IsOnline = false
-	frontendServer.Crashes = frontendServer.Crashes + 1
-
-	dataBaseServer.IsOnline = false
-	dataBaseServer.Crashes = dataBaseServer.Crashes + 1
-
-	// 5. OUTPUT NEW STATE
-	fmt.Println("Server Online:", frontendServer.IsOnline)
-	fmt.Println("Total Crashes:", frontendServer.Crashes)
-	fmt.Println("Server Online:", dataBaseServer.IsOnline)
-	fmt.Println("Total Crashes:", dataBaseServer.Crashes)
-	fmt.Printf("ALERT: %s at %s is currently offline!\n", frontendServer.Name, frontendServer.IPAddress)
-	fmt.Printf("ALERT: %s at %s is currently offline!\n", dataBaseServer.Name, dataBaseServer.IPAddress)
-
+	// 5. VERIFY RECOVERY
+	// If we didn't use the '*' pointer in step 1, this would still print 'false'!
+	fmt.Printf("\nFinal State -> %s Online: %t\n Crashes: %d\n", targetServer.Name, targetServer.IsOnline, targetServer.Crashes)
 }
